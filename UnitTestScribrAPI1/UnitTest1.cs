74e63bc1bb8fa1266604d8407fec81a1ea69c36c
jpat97 <jpatspatel97@outlook.com>
2019-07-22 12:47:04 +1200

    added test project

---
+++ UnitTestScribrAPI1/UnitTest1.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ScribrAPI.Model;
+
+namespace UnitTestScribrAPI1
+{
+
+   
+
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
